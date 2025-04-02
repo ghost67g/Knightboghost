49e3e2f02775549c0f2896fc2a570e83c415d824
ghost67g <vasudevvs074@gmail.com>
2025-04-02 23:19:49 +0530

    Update settings.jsg

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ghost',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ghost bot",
+  botOwner: 'Professor', // Your name ghost 
+  ownerNumber: '917023951514', //Your number 918301966072
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for managing group commands and automating tasksi am thinking.",
   version: "1.0.0",
 };
 
